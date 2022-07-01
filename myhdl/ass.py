30a665994648e348a0bfb61ee7aa5a73952c1fc7
Grissess <grissess@nexusg.org>
2022-07-01 14:48:32 -0400

    Add the CPU model (doesn't synth yet, though)

--- myhdl/ass.py
+++ myhdl/ass.py
@@ -1,9 +1,12 @@
 import random
 import os
+import traceback
+import sys
+import ast
 from random import randrange
 
-from myhdl import block, always_comb, \
-        Signal, intbv, modbv, delay, instance, \
+from myhdl import block, always_comb, always_seq, always, \
+        Signal, ResetSignal, intbv, modbv, delay, instance, \
         now, enum, bin
 
 SUBDIR = 'verilog'
@@ -89,6 +92,147 @@ def comp_unit(d, s, eq, gt, sn, iv, out):
 
     return comb
 
+CPU_STAGE = enum('IDLE', 'FETCH', 'EXEC', 'WRITEBACK')
+
+@block
+def cpu(addr, data, ready, valid, clk, halt, reset):
+    width = len(addr)
+
+    state = Signal(CPU_STAGE.IDLE)
+    d = Signal(modbv(0)[width:])
+    s = Signal(modbv(0)[width:])
+
+    l_op, l_out = Signal(intbv(0)[4:]), Signal(modbv(0)[width:])
+    logic = logic_unit(d, s, l_op, l_out)
+
+    a_op, a_out = Signal(ARITH.ADD), Signal(modbv(0)[width:])
+    arith = arith_unit(d, s, a_op, a_out)
+
+    c_eq, c_gt, c_sn, c_iv, c_out = map(Signal, [False] * 5)
+    comp = comp_unit(d, s, c_eq, c_gt, c_sn, c_iv, c_out)
+
+    inst = Signal(intbv(0)[16:])
+    npc = Signal(intbv(0)[width:])
+
+    reg = [Signal(modbv(0)[width:]) for _ in range(16)]
+
+    @always_seq(clk.posedge, reset=reset)
+    def tick():
+        # Keep the branches of this if unmerged; it helps MyHDL realize that
+        # it's actually a case analysis
+        if state == CPU_STAGE.IDLE:
+            if not halt:
+                state.next = CPU_STAGE.FETCH
+        elif state == CPU_STAGE.FETCH:
+            if valid:
+                ready.next = False
+                inst.next = data
+                state.next = CPU_STAGE.EXEC
+            else:
+                pc = reg[0]  # PC
+                addr.next = pc
+                npc.next = pc + 2
+                ready.next = True
+        elif state == CPU_STAGE.EXEC:
+            dl = inst[4:0]
+            sp = inst[8:4]
+            if inst[16:14] == 0b01:
+                pass  # TODO: data transfer
+            else:
+                code = inst[16:12]
+                if code == 0b0001:
+                    d.next = reg[dl]
+                    s.next = reg[sp]
+                    l_op.next = inst[12:8]
+                elif code == 0b0010:
+                    d.next = reg[dl]
+                    s.next = reg[sp]
+                    # FIXME: the Verilog synth can't deal with this swizzle
+                    #a_op.next = getattr(ARITH, ARITH._names[int(inst[11:8])])
+                    a_code = inst[11:8]
+                    if a_code == 0:
+                        a_op.next = ARITH.ADD
+                    elif a_code == 1:
+                        a_op.next = ARITH.SUB
+                    elif a_code == 2:
+                        a_op.next = ARITH.SHL
+                    elif a_code == 3:
+                        a_op.next = ARITH.SHR
+                    elif a_code == 4:
+                        a_op.next = ARITH.ASR
+                    elif a_code == 5:
+                        a_op.next = ARITH.MUL
+                    elif a_code == 6:
+                        a_op.next = ARITH.DIV
+                    elif a_code == 7:
+                        a_op.next = ARITH.MOD
+                elif code == 0b0011:
+                    d.next = reg[dl]
+                    s.next = reg[sp]
+                    c_eq.next = inst[8]
+                    c_gt.next = inst[9]
+                    c_sn.next = inst[10]
+                    c_iv.next = inst[11]
+                elif code == 0b1000:
+                    offset = inst[8:0]
+                    cmp = inst[12:8]
+                    if reg[cmp] != 0:
+                        npc.next = npc.signed() + offset.signed()
+                elif code == 0b1001:
+                    reg[dl].next = npc
+                    npc.next = reg[sp]
+            state.next = CPU_STAGE.WRITEBACK
+        elif state == CPU_STAGE.WRITEBACK:
+            dl = inst[4:0]
+            if inst[16:14] != 0b01:
+                code = inst[16:12]
+                if code == 0b0001:
+                    reg[dl].next = l_out
+                elif code == 0b0010:
+                    reg[dl].next = a_out
+                elif code == 0b0011:
+                    if c_out:
+                        reg[dl].next = 1
+                    else:
+                        reg[dl].next = 0
+            reg[0].next = npc  # PC
+            if halt:
+                state.next = CPU_STAGE.IDLE
+            else:
+                state.next = CPU_STAGE.FETCH
+
+    return logic, arith, comp, tick
+
+def try_convert(block):
+    try:
+        block.convert(path = SUBDIR)
+    except Exception:
+        traceback.print_exc()
+        _, _, tb = sys.exc_info()
+
+        print('-'*70)
+        print('This appears to have been during the processing of this/these AST node(s) (the innermost one(s) found):')
+
+        frames = []
+        cur = tb
+        while cur is not None:
+            frames.append(cur.tb_frame)
+            cur = cur.tb_next
+
+        for frame in reversed(frames):
+            lc = frame.f_locals
+            found = False
+            for name, val in lc.items():
+                if isinstance(val, ast.AST):
+                    found = True
+                    print(f'{name}:')
+                    print(ast.dump(val))
+                    print(f'(line {val.lineno})')
+            if found:
+                break
+
+        print('Continuing to your test anyway...')
+
 @block
 def logic_test(w, tries=10000):
     p = Signal(intbv(0)[w:])
@@ -97,7 +241,7 @@ def logic_test(w, tries=10000):
     out = Signal(intbv(0)[w:])
 
     unit = logic_unit(p, q, op, out)
-    unit.convert(path = SUBDIR)
+    try_convert(unit)
 
     @instance
     def temporal():
@@ -136,7 +280,7 @@ def arith_test(w, tries=10000):
     out = Signal(modbv(0)[w:])
 
     unit = arith_unit(d, s, op, out)
-    unit.convert(path = SUBDIR)
+    try_convert(unit)
 
     @instance
     def temporal():
@@ -173,7 +317,7 @@ def comp_test(w, tries=10000):
     out = Signal(False)
 
     unit = comp_unit(d, s, eq, gt, sn, iv, out)
-    unit.convert(path = SUBDIR)
+    try_convert(unit)
 
     @instance
     def temporal():
@@ -199,6 +343,46 @@ def comp_test(w, tries=10000):
                 assert out == res
 
     return unit, temporal
+
+@block
+def cpu_ram(words, addr, data, ready, valid):
+    @always_comb
+    def comb():
+        if ready:
+            data.next = words[int(addr) >> 1]
+            valid.next = True
+        else:
+            valid.next = False
+    return comb
+
+@block
+def cpu_test(w, _ignored=0):
+    addr = Signal(modbv(0)[w:])
+    data = Signal(modbv(0)[16:])
+    ready = Signal(False)
+    valid = Signal(False)
+    clk = Signal(False)
+    halt = Signal(False)
+    reset = ResetSignal(True, active=True, isasync=True)
+
+    cpu_unit = cpu(addr, data, ready, valid, clk, halt, reset)
+    try_convert(cpu_unit)
+    mem = cpu_ram(
+            [0x1322, 0x1a21, 0x82fc],
+            addr, data, ready, valid,
+    )
+
+    @instance
+    def temporal():
+        # Allow a reset to occur
+        yield delay(1)
+        reset.next = False
+        for step in range(48):
+            print(f'{now()}:\t{"tick" if clk else "tock"}\taddr={addr}\tdata={data}\tvalid={valid}\tready={ready}\tstate={cpu_unit.sigdict["state"]}\tinst={cpu_unit.sigdict["inst"]}\tregs={" ".join(bin(i.val, w) for i in cpu_unit.symdict["reg"])}')
+            clk.next = not clk
+            yield delay(1)
+
+    return cpu_unit, mem, temporal
 
 if __name__ == '__main__':
     import sys
