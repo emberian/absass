fc8b1f30c2d1a9a34c7f5ab92c586a893ebd55dc
ember arlynx <ember@lunar.town>
2022-07-13 05:39:38 -0400

    rework ftdi a lot

--- spinal_ass/src/main/scala/mylib/Ftdi.scala
+++ spinal_ass/src/main/scala/mylib/Ftdi.scala
@@ -4,7 +4,7 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.fsm._
 
-class Ftdi extends Component {
+class Ftdi(val fifo_sz: Int) extends Component {
   val Byte = UInt(8 bits)
   val io = new Bundle {
     val rd = master Stream (Byte)
@@ -16,93 +16,135 @@ class Ftdi extends Component {
     val rxd = in Bool ()
   }
 
-  val spiclk = new ClockDomain(ftdi.tck, clockDomain.reset, frequency = 6 MHz)
+  ftdi.txd.assignDontCare()
 
-  val spiDomain = new ClockingArea(spiclk) {
-    val buf = Reg(Byte) init (0) addTag (crossClockDomain)
-    val buf_rdy = Reg(Bool) init (False) addTag (crossClockDomain)
+  val baud = 9600 Hz
 
-    val main = new StateMachine {
-      val bits_sent = Reg(UInt(5 bits)) init (0)
-      val fairness = Reg(Bool()) init (False)
-      val cmdbuf = Reg(UInt(24 bits)) init (0)
+  val baudClk = RegInit(False)
+  val rxClk = RegInit(False)
 
-      val start = new State with EntryPoint {
-        onEntry {
-          bits_sent := 0; buf := 0; wr.ready := True
-        }
+  val baudDom = ClockDomain(baudClk, clockDomain.reset)
+  val rDom = ClockDomain(rxClk, clockDomain.reset)
+
+  val rdfifo = StreamFifoCC(
+    dataType = Byte,
+    depth = fifo_sz,
+    pushClock = rDom,
+    popClock = clockDomain
+  )
+  rdfifo.io.push.valid := False
+  rdfifo.io.push.payload.assignDontCare()
+
+  val wrfifo = StreamFifoCC(
+    dataType = Byte,
+    depth = fifo_sz,
+    pushClock = clockDomain,
+    popClock = baudDom
+  )
+
+  wrfifo.io.pop.ready := False
+
+  rdfifo.io.pop >> io.rd
+  wrfifo.io.push << io.wr
+
+  val sync_clk = Reg(Bool()) init (False) addTag (crossClockDomain)
+
+  val rctl = new Area {
+    val currentFreq = ClockDomain.current.frequency.getValue.toBigDecimal
+    val factor = (currentFreq / (2 * baud.toBigDecimal)).toInt
+    // multiply by two to ensure one edge per baud
+    val rdctr = Reg(UInt(log2Up(factor) bits)) init (0)
+    val wrctr = Reg(UInt(log2Up(factor) bits)) init (0)
+    // we thus sample in the middle of the baud period
+    when(!ftdi.rxd && sync_clk) {
+      rdctr := factor / 2
+      rxClk := False
+    } otherwise {
+      rdctr := rdctr + 1
+    }
+    when(rdctr === factor - 1) { rxClk := !rxClk; rdctr := 0 }
+    when(wrctr === factor - 1) { baudClk := !baudClk; wrctr := 0 }
+  }
+
+  val rxar = new ClockingArea(rDom) {
+    val sent = RegInit(False)
+    when(rdfifo.io.push.ready && sent) {
+      rdfifo.io.push.valid := False; sent := False
+    }
+
+    val rx = new StateMachine {
+      val recvbf = Reg(UInt(8 bits)) init (0)
+      val bits_recvd = Reg(UInt(3 bits)) init (0)
+
+      val idle: State = new State with EntryPoint {
         whenIsActive {
-          buf_rdy := False
-          when(rd.ready) {
-            when(fairness) {
-              when(wr.valid) {
-                goto(startWrite)
-                // they had their chance
-                fairness := False
-              } otheriwse {
-                goto(startRead)
-              }
-            } otherwise {
-              // it our turn! but be nice next time
-              fairness := True
-              goto(startRead)
-            }
-          } otherwise {
-            when(wr.valid) {
-              goto(startWrite)
-            }
+          when(!ftdi.rxd) {
+            goto(rd)
           }
         }
       }
 
-      val startWrite = new State {
-        onEntry {
-          wr.ready := False; cmdbuf(23 downto 16) := 0x22;
-          cmdbuf(15 downto 8) := 7; cmdbuf(7 downto 0) := wr.payload
-        }
+      val rd = new State {
+        // god i hope these clock phases are good enough
+        onEntry { bits_recvd := 0 }
+        onExit { sync_clk := True }
         whenIsActive {
-          // cross our fingers about this clock domain?
-          ftdi.tdo := cmdbuf(23)
-          cmdbuf := cmdbuf << 1
-          bits_sent := bits_sent + 1
-          when(bits_sent === 23) {
-            goto(start)
+          bits_recvd := bits_recvd + 1
+          recvbf(7) := ftdi.rxd
+          recvbf(6 downto 0) := recvbf(7 downto 1)
+
+          when(bits_recvd === 7) {
+            rdfifo.io.push.valid := True
+            sent := True
+            // what does a stall do? if sent = true, we stalled.
+            rdfifo.io.push.payload := recvbf
+            goto(idle)
           }
-        }
-      }
-
-      val startRead = new State {
-        onEntry {
-          cmdbuf(23 downto 16) := 0x2A
-          cmdbuf(15 downto 8) := 7
-        }
-
-        whenIsActive {
-            ftdi.tdo := cmdbuf(23)
-            cmdbuf := cmdbuf << 1
-            bits_sent := bits_sent + 1
-            when(bits_sent === 15) {
-              goto(continueRead)
-            }
-        }
-      }
-
-      val continueRead = new State {
-        onEntry { bits_sent := 0 }
-        whenIsActive {
-            buf := (buf << 1) | ftdi.tdi
-            bits_sent := bits_sent + 1
-            when (bits_sent === 7) {
-                buf_rdy := True
-                goto(start)
-            }
         }
       }
     }
   }
 
-  when(spiDomain.buf_rdy) {
-    rd.payload := spiDomain.buf
-    rd.valid := True
+  val txar = new ClockingArea(baudDom) {
+    val tx = new StateMachine {
+      val bits_sent = Reg(UInt(4 bits)) init (0)
+      val dataw = Reg(UInt(10 bits)) init (0)
+
+      val idle: State = new State with EntryPoint {
+        onEntry { wrfifo.io.pop.ready := True }
+
+        whenIsActive {
+          when(wrfifo.io.pop.valid) { goto(wr) }
+        }
+        onExit { wrfifo.io.pop.ready := False }
+      }
+
+      val wr = new State {
+        onEntry {
+          dataw(0) := False
+          dataw(9) := True
+          dataw(8 downto 1) := wrfifo.io.pop.payload
+          bits_sent := 0
+        }
+        whenIsActive {
+          ftdi.txd := dataw(0)
+          dataw(6 downto 0) := dataw(7 downto 1)
+          bits_sent := bits_sent + 1
+          when(bits_sent === 9) {
+            goto(idle)
+          }
+        }
+      }
+    }
+  }
+}
+
+object FtdiV {
+  def main(args: Array[String]) {
+    SpinalConfig(
+      mode = Verilog,
+      defaultClockDomainFrequency = FixedFrequency(450 MHz)
+    )
+      .generate(new Ftdi(2))
   }
 }
